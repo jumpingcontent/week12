5d11d7f79c8357978ed3372cf50bfcb9ef3792fb
Ron <35547141+jumpingcontent@users.noreply.github.com>
2018-04-04 22:53:55 -0500

    Update tcpdump.sh

--- tcpdump.sh
+++ tcpdump.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
+# View all interfaces
 tcpdump -i any
+
+# View whats on eth0 interface if it exists
+tcpdump -i eth0
+
+# View traffic on http protocol
+tcpdump port 80
+
+# Displaying the available interfaces
+tcpdump -D
+
+# Capture just tcp traffic
+tcpdump -i eth0 tcp
+
+# Capture traffic within a port range
+tcpdump portrange 80-443
+
+# Capture packets based on byte size
+tcpdump greater 64
